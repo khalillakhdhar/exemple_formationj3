95996a761080eb9eed983cea6fcdee6db760a2a2
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-06-30 09:24:01 +0100

    controller

--- src/main/java/com/example/demo/controller/FormationController.java
+++ src/main/java/com/example/demo/controller/FormationController.java
@@ -5,13 +5,17 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entities.Formation;
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.FormationRepository;
 
 @RestController
@@ -30,5 +34,19 @@ public class FormationController {
 	{
 		return formationRepository.save(formation);
 		
+	}
+	@GetMapping("/{id}")
+	public Formation getFormationById(@PathVariable(value = "id") Long idformation)
+	{
+	return formationRepository.findById(idformation).orElseThrow(()->new ResourceNotFoundException("formation", "id", idformation));	
+		
+		
+	}
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deleteFormation(@PathVariable(value="id") Long idformation)
+	{
+		Formation formation=formationRepository.findById(idformation).orElseThrow(()->new ResourceNotFoundException("formation", "id", idformation));
+		formationRepository.delete(formation);
+		return ResponseEntity.ok().build();
 	}
 }
