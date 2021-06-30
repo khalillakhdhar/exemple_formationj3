9a179d84e478ce4dd411a2dfbd2469174c01765d
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-06-30 08:42:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# exemple_formationj3
+ 
---
+++ src/main/java/com/example/demo/ExempleFormationj3Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExempleFormationj3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExempleFormationj3Application.class, args);
+	}
+
+}
